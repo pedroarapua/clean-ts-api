1e9e326ee4f035b53cfc50d588fc385bdee31374
Pedro Henrique Fernandes Dias <pedro.dias@luizalabs.com>
2021-02-26 21:48:37 -0300

    feat: ensure SignUpController returns 400 if no email is provided

--- src/entrypoints/controllers/signup.ts
+++ src/entrypoints/controllers/signup.ts
@@ -1,8 +1,18 @@
 export class SignUpController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missiing param: name')
+    const { name, email } = httpRequest.body
+    if (!name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missiing param: name')
+      }
+    }
+
+    if (!email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missiing param: email')
+      }
     }
   }
 }
