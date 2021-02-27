29b3a368c6f100aae0b180cb7bd13374113762f9
Pedro Henrique Fernandes Dias <pedro.dias@luizalabs.com>
2021-02-27 14:18:48 -0300

    chore: add jest configs

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,9 @@ module.exports = {
     '<rootDir>/src'
   ],
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**'
   ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
