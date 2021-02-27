11a9d4c98e1c0903194a743aa3a743a47977bd95
Pedro Henrique Fernandes Dias <pedro.dias@luizalabs.com>
2021-02-27 13:12:54 -0300

    feat: ensure SignUpController calls AddAccount with correct values

---
+++ src/core/usecases/add-account.ts
@@ -0,0 +1,11 @@
+import { AccountModel } from '../entities/account'
+
+export interface AddAccountModel {
+  name: string
+  email: string
+  password: string
+}
+
+export interface AddAccount {
+  add: (account: AddAccountModel) => AccountModel
+}
